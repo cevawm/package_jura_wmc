84e98071034ed356ebd20f9cebbe8aabbbeee81c
Will Ceva <william.ceva@unige.ch>
2023-05-09 10:07:26 +0200

    Same as previous commit but with a test at the end

--- rv_curve_module.py
+++ rv_curve_module.py
@@ -51,3 +51,15 @@ class rv_curve_class:
         nu = self.true_anomaly()                         # get true anomaly from true_anomaly function
         rv = self.k * np.cos(nu + self.w) + self.e * np.cos(self.w)  # compute RV curve
         return self.t_vector, rv
+    
+# create instance of the class
+rv = rv_curve_class(t0=0., p=8000., e=0.5, w=np.pi/3, k=500., t_init=0., t_end=10000.)
+
+# compute the RV curve
+t, rv = rv.rv_curve()
+
+# plot the RV curve
+plt.plot(t, rv)
+plt.xlabel("Time [d]")
+plt.ylabel("RV [m/s]")
+plt.show()
